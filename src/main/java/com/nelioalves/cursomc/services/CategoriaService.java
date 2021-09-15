3250d4e48169bf2123f6d80ffaf3f950aa0446a5
Mateus Miranda <mateus.m.taio@gmail.com>
2021-09-15 11:52:23 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
@@ -51,4 +54,9 @@ public class CategoriaService {
 		return repo.findAll();
 	}
 
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction.toUpperCase()), orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
 }
